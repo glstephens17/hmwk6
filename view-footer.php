3b9793d52034b96dd6bc55b7edd7d7caeea2f428
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-11-15 13:41:10 -0600

    Update view-footer.php

--- view-footer.php
+++ view-footer.php
@@ -51,6 +51,22 @@
             }, 3000);
         }
     </script>
+    <script>const config = {
+  type: 'doughnut',
+  data: data,
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Doughnut Chart'
+      }
+    }
+  },
+};</script>
     <!DOCTYPE html>
 <html>
 <body>
