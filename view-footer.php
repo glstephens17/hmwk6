729e0c118b88fcbb5a1ac0ab4c6382a8b519c991
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-11-15 13:39:44 -0600

    Update view-footer.php

--- view-footer.php
+++ view-footer.php
@@ -29,22 +29,6 @@
             background-color: #0056b3;
         }
     </style>
-    const config = {
-  type: 'doughnut',
-  data: data,
-  options: {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-      },
-      title: {
-        display: true,
-        text: 'Chart.js Doughnut Chart'
-      }
-    }
-  },
-};
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <body>
     <div class="container">
